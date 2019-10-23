c66970f504a90c0533b31e80ca666e730c4dc2e3
Michael Espina <michael_espina@yahoo.com>
2019-10-23 07:01:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -17,8 +17,14 @@ def join_ingredients(src)
         if array_1[row_index][element_index].count % 2 == 0
           inner_array_element0 = array_1[row_index][element_index]
         else
-          inner_array_element1 = 
-        inner_results << "I love (inner array element 0) and (inner array element 1) on my pizza"
+          inner_array_element1 = array_1[row_index][element_index]
+        end
+        inner_results << "I love #{inner_array_element0} and #{inner_array_element1} on my pizza"
+      end
+      element_index += 1
+    end
+    src << inner_results
+    row_index += 1
 end
 
 def find_greater_pair(src)
